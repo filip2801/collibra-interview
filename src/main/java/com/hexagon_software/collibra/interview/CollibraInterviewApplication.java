4839a081bb8e1aa2baa4d0c8c0b5feb8817319e9
Filip Karaś <filip2801@gmail.com>
2017-03-24 23:29:04 +0100

    Fix test

--- src/main/java/com/hexagon_software/collibra/interview/CollibraInterviewApplication.java
+++ src/main/java/com/hexagon_software/collibra/interview/CollibraInterviewApplication.java
@@ -10,14 +10,12 @@ import org.apache.mina.filter.codec.ProtocolCodecFilter;
 import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
 import org.apache.mina.filter.logging.LoggingFilter;
 import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class CollibraInterviewApplication {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         IoAcceptor acceptor = new NioSocketAcceptor();
-        acceptor.getFilterChain().addLast( "codec", new ProtocolCodecFilter( new TextLineCodecFactory( Charset.forName( "UTF-8" ))));
+        acceptor.getFilterChain().addLast("codec", new ProtocolCodecFilter(new TextLineCodecFactory(Charset.forName("UTF-8"))));
         acceptor.getFilterChain().addLast("logger", new LoggingFilter());
         acceptor.setHandler(new SimpleHandler());
         acceptor.getSessionConfig().setIdleTime(IdleStatus.READER_IDLE, 30);
