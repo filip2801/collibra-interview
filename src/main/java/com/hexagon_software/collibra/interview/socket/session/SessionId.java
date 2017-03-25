9a0718e75d3e8a08c79095d029a3cdc832591c66
Filip Karaś <filip2801@gmail.com>
2017-03-25 18:19:50 +0100

    SessionId with lombok Value annotation

--- src/main/java/com/hexagon_software/collibra/interview/socket/session/SessionId.java
+++ src/main/java/com/hexagon_software/collibra/interview/socket/session/SessionId.java
@@ -1,8 +1,10 @@
 package com.hexagon_software.collibra.interview.socket.session;
 
-import java.util.Objects;
 import java.util.UUID;
 
+import lombok.Value;
+
+@Value
 public class SessionId {
 
     private final UUID uuid;
@@ -14,25 +16,6 @@ public class SessionId {
     @Override
     public String toString() {
         return uuid.toString();
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) {
-            return true;
-        }
-
-        if (object == null || getClass() != object.getClass()) {
-            return false;
-        }
-
-        SessionId sessionId = (SessionId) object;
-        return Objects.equals(uuid, sessionId.uuid);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(uuid);
     }
 
 }
