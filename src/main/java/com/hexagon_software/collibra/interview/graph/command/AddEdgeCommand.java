e5f97fe3ddfa189f6f6a5e2c1440f5ecaa1200e4
Filip Karaś <filip2801@gmail.com>
2017-03-26 11:31:49 +0200

    AddEdgeCommand validation

--- src/main/java/com/hexagon_software/collibra/interview/graph/command/AddEdgeCommand.java
+++ src/main/java/com/hexagon_software/collibra/interview/graph/command/AddEdgeCommand.java
@@ -1,18 +1,24 @@
 package com.hexagon_software.collibra.interview.graph.command;
 
 import com.hexagon_software.collibra.interview.graph.model.NodeName;
-import lombok.NonNull;
 import lombok.Value;
+import org.apache.commons.lang3.Validate;
 
 @Value
 public class AddEdgeCommand {
 
-    @NonNull
     NodeName start;
-
-    @NonNull
     NodeName end;
-
     int weight;
+
+    public AddEdgeCommand(NodeName start, NodeName end, int weight) {
+        Validate.notNull(start, "start is null");
+        Validate.notNull(end, "end is null");
+        Validate.isTrue(weight >= 0, "weight is negative");
+
+        this.start = start;
+        this.end = end;
+        this.weight = weight;
+    }
 
 }
