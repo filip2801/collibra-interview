91c36d4ffcd1fced976d0d0e5c62844f59faee47
Filip Karaś <filip2801@gmail.com>
2017-03-27 02:02:05 +0200

    Javadocs

--- src/main/java/com/hexagon_software/collibra/interview/handler/MessageHandler.java
+++ src/main/java/com/hexagon_software/collibra/interview/handler/MessageHandler.java
@@ -4,6 +4,12 @@ public interface MessageHandler {
 
     boolean isSupported(Object message);
 
+    /**
+     * Handle message. Method could throw unchecked exception if handling is not supported.
+     *
+     * @param message message
+     * @return response message
+     */
     Response handle(Object message);
 
 }
--- src/main/java/com/hexagon_software/collibra/interview/socket/handler/SessionAwareMessageHandler.java
+++ src/main/java/com/hexagon_software/collibra/interview/socket/handler/SessionAwareMessageHandler.java
@@ -6,6 +6,12 @@ public interface SessionAwareMessageHandler {
 
     boolean isSupported(IoSession session, Object message);
 
+    /**
+     * Handle message. Method could throw unchecked exception if handling is not supported.
+     *
+     * @param session session
+     * @param message message
+     */
     void handle(IoSession session, Object message);
 
 }
