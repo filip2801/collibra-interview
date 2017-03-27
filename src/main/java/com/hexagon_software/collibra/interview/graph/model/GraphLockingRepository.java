c9e0264d4f50c1367d0b5f41e304a7e4d6b17b4d
Filip Karaś <filip2801@gmail.com>
2017-03-27 02:08:17 +0200

    Duplicated javadocs removed

--- src/main/java/com/hexagon_software/collibra/interview/graph/model/GraphLockingRepository.java
+++ src/main/java/com/hexagon_software/collibra/interview/graph/model/GraphLockingRepository.java
@@ -28,51 +28,26 @@ public class GraphLockingRepository implements GraphRepository {
         this.closerThanResolver = closerThanResolver;
     }
 
-
     @PostConstruct
     synchronized void initGraph() {
         graph = new Graph();
     }
 
-    /**
-     * Add node to graph.
-     *
-     * @param nodeName node name
-     * @return <code>true</code> if node added, <code>false</code> if node not added because node with same name already exists
-     */
     @Override
     public synchronized boolean addNode(NodeName nodeName) {
         return graph.addNode(nodeName);
     }
 
-    /**
-     * Add edge to graph.
-     *
-     * @param command add edge command
-     * @return <code>true</code> if edge added, <code>false</code> if node not found
-     */
     @Override
     public synchronized boolean addEdge(AddEdgeCommand command) {
         return graph.addEdge(command);
     }
 
-    /**
-     * Remove node from graph.
-     *
-     * @param nodeName node name
-     * @return <code>true</code> if node removed, <code>false</code> if node not found
-     */
     @Override
     public synchronized boolean removeNode(NodeName nodeName) {
         return graph.removeNode(nodeName);
     }
 
-    /**
-     * Remove edge from graph.
-     *
-     * @param command remove edge command
-     * @throws NodeNotFound if node not found
-     */
     @Override
     public synchronized void removeEdges(RemoveEdgeCommand command) throws NodeNotFound {
         graph.removeEdges(command);
